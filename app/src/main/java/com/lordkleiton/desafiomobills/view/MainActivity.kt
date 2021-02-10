b1d221a9cfba36da48347ccc20799fc567fd10fb
lord <m_s_r@outlook.com>
2021-02-10 20:28:35 -0300

    adicionando fragments

--- app/src/main/java/com/lordkleiton/desafiomobills/view/MainActivity.kt
+++ app/src/main/java/com/lordkleiton/desafiomobills/view/MainActivity.kt
@@ -2,14 +2,49 @@ package com.lordkleiton.desafiomobills.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.lordkleiton.desafiomobills.R
+import com.lordkleiton.desafiomobills.databinding.ActivityMainBinding
+import com.lordkleiton.desafiomobills.view.fragment.ExpensesFragment
+import com.lordkleiton.desafiomobills.view.fragment.HomeFragment
+import com.lordkleiton.desafiomobills.view.fragment.IncomesFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        switchFragments(HomeFragment())
+
+        setupNavigation()
     }
 
+    private fun setupNavigation() {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            val fragment = when (it.itemId) {
+                R.id.menu_item_home -> HomeFragment()
+                R.id.menu_item_income -> IncomesFragment()
+                else -> ExpensesFragment()
+            }
+
+            switchFragments(fragment)
+
+            true
+        }
+    }
+
+    private fun switchFragments(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragments_container, fragment)
+
+            commit()
+        }
+    }
+
+    //impedindo de voltar pra tela de login
     override fun onBackPressed() {
         moveTaskToBack(true)
     }
