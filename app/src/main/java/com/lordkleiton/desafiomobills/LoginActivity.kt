5d2a6893dfa813e3bb4e5ce4d5db4285e5fea5f2
Mario <mario@mobills.com.br>
2021-02-10 16:41:40 -0300

    refatoração de login

--- app/src/main/java/com/lordkleiton/desafiomobills/LoginActivity.kt
+++ app/src/main/java/com/lordkleiton/desafiomobills/LoginActivity.kt
@@ -3,8 +3,8 @@ package com.lordkleiton.desafiomobills
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -12,88 +12,122 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.lordkleiton.desafiomobills.databinding.ActivityLoginBinding
-import com.lordkleiton.desafiomobills.model.Despesa
 import com.lordkleiton.desafiomobills.util.AppConst.LOGIN_REQUEST_CODE
-import com.lordkleiton.desafiomobills.viewmodel.ExpensesViewModel
+import com.lordkleiton.desafiomobills.util.AppConst.TAG
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private val db = Firebase.firestore
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var client: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        val root = binding.root
-        setContentView(root)
+        setContentView(binding.root)
 
-        auth = FirebaseAuth.getInstance()
+        setupAuth()
 
+        setupButton()
+
+//        val vm = ViewModelProvider(this).get(ExpensesViewModel::class.java)
+//
+//        vm.save(Despesa()).observe(this, {
+//            Log.i("hmm", it.toString())
+//        })
+    }
+
+    private fun setupAuth() {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
 
-        binding.activityMainButton.setOnClickListener {
-            signIn()
+        client = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun setupButton() {
+        binding.loginButton.apply {
+            setOnClickListener {
+                isClickable = false
+
+                hideMsg()
+
+                changeLoading(View.VISIBLE)
+
+                signIn()
+            }
         }
+    }
 
-        val vm = ViewModelProvider(this).get(ExpensesViewModel::class.java)
-
-        vm.save(Despesa()).observe(this, {
-            Log.i("hmm", it.toString())
-        })
+    private fun changeLoading(visibility: Int) {
+        binding.loginLoading.visibility = visibility
     }
 
     private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, LOGIN_REQUEST_CODE)
+        val intent = client.signInIntent
+
+        startActivityForResult(intent, LOGIN_REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == LOGIN_REQUEST_CODE) {
+
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
             try {
                 val account = task.getResult(ApiException::class.java)!!
-                Log.d("hmm", "firebaseAuthWithGoogle:" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
+
+                Log.d(TAG, "userId: ${account.id}")
+
+                auth(account.idToken!!)
             } catch (e: ApiException) {
-                Log.w("hmm", "Google sign in failed", e)
+                onError(e)
             }
         }
     }
 
-    private fun firebaseAuthWithGoogle(idToken: String) {
+    private fun onError(e: Exception) {
+        Log.w(TAG, "auth failed: ", e)
+
+        showMsg()
+
+        changeLoading(View.INVISIBLE)
+
+        binding.loginButton.isClickable = true
+    }
+
+    private fun hideMsg() {
+        binding.loginError.visibility = View.INVISIBLE
+    }
+
+    private fun showMsg() {
+        binding.loginError.apply {
+            visibility = View.VISIBLE
+
+            text = resources.getString(R.string.error_login)
+        }
+    }
+
+    private fun auth(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
 
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    Log.d("hmm", "signInWithCredential:success")
+                    auth.currentUser?.apply {
+                        Log.d(TAG, "userName: $displayName")
 
-                    val user = auth.currentUser
+                        Snackbar.make(binding.root, displayName ?: "", Snackbar.LENGTH_SHORT).show()
 
-                    if (user != null) {
-                        val view = binding.root
-
-                        Snackbar.make(view, user.displayName ?: "", Snackbar.LENGTH_SHORT).show()
+                        changeLoading(View.INVISIBLE)
                     }
-
                 } else {
-                    Log.w("hmm", "signInWithCredential:failure", task.exception)
-
-                    val view = binding.root
-
-                    Snackbar.make(view, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                    onError(task.exception!!)
                 }
             }
     }
